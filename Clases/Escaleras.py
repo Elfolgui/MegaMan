45c066abff3ef787c75063b79f6bac04b0074d4a
Elfolgui <aguante.los.rulos@gmail.com>
2017-10-03 20:19:47 -0300

    N

--- Clases/Escaleras.py
+++ Clases/Escaleras.py
@@ -1,11 +1,8 @@
 from .Base import *
 
-
-
 class Escalera(Base):
 
     def __init__(self, x, y, ancho, alto, ruta):
         Base.__init__(self, x, y, ancho, alto, ruta)
-        Base.sprites.add(self)
         Base.Escalera.add(self)
         self.Tipo = "Escalera"
--- Clases/dibujo.py
+++ Clases/dibujo.py
@@ -3,5 +3,6 @@ from . import *
 def dibujo(fondo, ventana, colores):
 
     Controlador.rellenar_pantalla(ventana, fondo, colores)
+    Base.Escalera.draw(ventana)
     Base.sprites.draw(ventana)
     pygame.display.flip()
